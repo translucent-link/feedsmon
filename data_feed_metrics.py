f99d1a64b3294d0f2e2fb6300c73ee3e3065e7c7
Ijonas Kisselbach <ijonas@ijonas.com>
2023-07-15 09:13:19 +0100

    support for response time metrics

--- data_feed_metrics.py
+++ data_feed_metrics.py
@@ -1,9 +1,11 @@
-from prometheus_client import Counter, Gauge, Info, start_http_server
+from prometheus_client import Counter, Gauge, Info
 
 metrics = {
     "data_feed": Gauge("data_feed", "Numeric data feed", ["project", "code"]),
+    "data_feed_response_time_ms": Gauge("data_feed_response_time", "Response time of data feed in milliseconds", ["project", "code"]),
     "data_feed_last_error": Info(
-        "data_feed_last_error", "Most recent error detected", ["project", "code"]
+        "data_feed_last_error", "Most recent error detected", [
+            "project", "code"]
     ),
     "data_feed_error_count": Counter(
         "data_feed_error_count",
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import datetime
 import time
+from prometheus_client import start_http_server
 
 import pyjq
 import requests
 
 from cfg import load_config
+from data_feed_metrics import metrics
 
 
 def init_metrics(data_feed_sources):
-    for data_feed_source in data_feed_sources:
+    for data_feed_source in data_feed_sources['data_source_metrics']:
         metrics["data_feed"].labels(
+            data_feed_source["project"], data_feed_source["code"]
+        ).set(0)
+        metrics["data_feed_response_time_ms"].labels(
             data_feed_source["project"], data_feed_source["code"]
         ).set(0)
         metrics["data_feed_last_error"].labels(
@@ -21,13 +26,14 @@ def init_metrics(data_feed_sources):
 
 
 def process_request(data_feed_sources, t):
-    for data_feed_source in data_feed_sources:
+    for data_feed_source in data_feed_sources['data_source_metrics']:
         try:
+            request_datetime = datetime.datetime.now()
             response = requests.get(
                 data_feed_source["url"], headers=data_feed_source["headers"]
             )
+            current_datetime = datetime.datetime.now()
             if response.status_code != 200:
-                current_datetime = datetime.datetime.now()
                 metrics["data_feed_last_error"].labels(
                     data_feed_source["project"], data_feed_source["code"]
                 ).info(
@@ -42,7 +48,11 @@ def process_request(data_feed_sources, t):
                 ).inc()
             else:
                 data = response.json()
-
+                duration = (current_datetime -
+                            request_datetime).total_seconds() * 1000
+                metrics["data_feed_response_time_ms"].labels(
+                    data_feed_source["project"], data_feed_source["code"]
+                ).set(duration)
                 price_str = pyjq.first(data_feed_source["jq"], data)
                 price = float(price_str) / 10 ** data_feed_source["decimals"]
                 metrics["data_feed"].labels(
